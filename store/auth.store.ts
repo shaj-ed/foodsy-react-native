976cf4f1f68f230a8b4caa29622ca9233bae97ea
shaj-ed <shajedsr@gmail.com>
2025-12-26 18:54:31 +0600

    Feat: function for login and registration and logout

--- store/auth.store.ts
+++ store/auth.store.ts
@@ -1,7 +1,7 @@
 import api from "@/src/api/axios";
 import { getCurrentUser } from "@/src/api/user";
-import { LoginResponse, UserType } from "@/src/types/auth";
-import { saveRefreshToken } from "@/src/utils/token";
+import { LoginResponse, SignUpPayload, UserType } from "@/src/types/auth";
+import { deleteRefreshToken, saveRefreshToken } from "@/src/utils/token";
 import { create } from "zustand";
 
 interface AuthState {
@@ -11,9 +11,13 @@ interface AuthState {
 
   setUser: (user: UserType) => void;
   login: (username: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+  signUp: (payload: SignUpPayload) => Promise<void>;
 }
 
 const AUTH_LOGIN_URL = "/auth/login";
+const AUTH_LOGOUT_URL = "/auth/logout";
+const AUTH_SIGNUP_URL = "/auth/register";
 
 export const useAuthStore = create<AuthState>((set) => ({
   user: null,
@@ -51,6 +55,31 @@ export const useAuthStore = create<AuthState>((set) => ({
         user: currentUser.data,
         isAuthenticated: true,
       });
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  },
+
+  logout: async () => {
+    try {
+      await api.post(AUTH_LOGOUT_URL);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      set({
+        user: null,
+        accessToken: null,
+        isAuthenticated: false,
+      });
+
+      await deleteRefreshToken();
+    }
+  },
+
+  signUp: async (payload) => {
+    try {
+      await api.post(AUTH_SIGNUP_URL, payload);
     } catch (error) {
       console.log(error);
       throw error;
