074a8f94f9e941eb90ab2ed42d46e2a68df75b46
shaj-ed <shajedsr@gmail.com>
2025-12-26 18:56:49 +0600

    Chore: add neccessary packages like and config changes a bit

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,20 +1,17 @@
 import CartButton from "@/components/CartButton";
-import { offers } from "@/constants";
+import SafeScreen from "@/components/common/SafeScreen";
+import Category from "@/features/category/components/Category";
 import { useAuthStore } from "@/store/auth.store";
-import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
-import cn from "clsx";
 import { Redirect } from "expo-router";
-import { FlatList, Image, Pressable, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, View } from "react-native";
 
 export default function Index() {
-  const tabBarHeight = useBottomTabBarHeight();
   const isAuthenticated = useAuthStore((s) => s.isAuthenticated);
 
   if (!isAuthenticated) return <Redirect href="/sign-in" />;
 
   return (
-    <SafeAreaView>
+    <SafeScreen>
       <View className="px-5 my-3 flex-row justify-between items-center">
         <View>
           <Text className="text-md text-amber-700 font-semibold">
@@ -24,49 +21,7 @@ export default function Index() {
         <CartButton />
       </View>
 
-      <FlatList
-        data={offers}
-        renderItem={({ item, index }) => {
-          const isEven = index % 2 === 0;
-
-          return (
-            <View>
-              <Pressable
-                style={{ backgroundColor: item.color }}
-                className={cn(
-                  "w-full h-48 flex gap- justify-between items-center overflow-hidden my-2 rounded-xl",
-                  isEven ? "flex-row-reverse" : "flex-row",
-                )}
-              >
-                {({ pressed }) => (
-                  <>
-                    <View className="h-full w-1/2">
-                      <Image
-                        source={item.image}
-                        className="size-full"
-                        resizeMode="contain"
-                      />
-                    </View>
-
-                    <View>
-                      <Text
-                        className={cn(
-                          "text-2xl leading-tight font-bold text-white ",
-                          isEven ? "pl-10" : "pr-10",
-                        )}
-                      >
-                        {item.title}
-                      </Text>
-                    </View>
-                  </>
-                )}
-              </Pressable>
-            </View>
-          );
-        }}
-        contentContainerClassName="px-5"
-        contentContainerStyle={{ paddingBottom: tabBarHeight + 120 }}
-      />
-    </SafeAreaView>
+      <Category />
+    </SafeScreen>
   );
 }
